c9961c46b7a1f94577d167537b90b03fa38bda24
yuekami <yuekami7@gmail.com>
2025-10-22 23:14:04 +0900

    PostモデルにRansack設定を追加し、検索可能な属性と関連を定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 1000 }
+
+  # Ransack設定：検索可能な属性を定義
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title description]
+  end
+
+  # Ransack設定：検索可能な関連を定義
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
