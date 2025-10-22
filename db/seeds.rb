8dc30d7878a4d35e4d831bcec16cf3783f9e20c0
yuekami <yuekami7@gmail.com>
2025-10-22 22:39:13 +0900

    Postのseedsを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Post用のサンプルデータを作成
+30.times do |i|
+  Post.find_or_create_by!(
+    title: "サンプル投稿 #{i + 1}",
+    description: "これは#{i + 1}番目のサンプル投稿です。Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  )
+end
+
+puts "30件のPostを作成しました。"
